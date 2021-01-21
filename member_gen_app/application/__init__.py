acc4a975cf4ba42bd180f6f1492beea79e70057b
Adam Ma <adammbq@gmail.com>
2021-01-21 14:35:38 +1100

    removed images

--- member_gen_app/application/__init__.py
+++ member_gen_app/application/__init__.py
@@ -6,16 +6,16 @@ print(cwd)
 
 
 # Things to modify
-UPLOAD_FOLDER = "/Users/sdce/Desktop/acgl/membership_card_gen/member_gen_app/application/uploads"
+UPLOAD_FOLDER = "/Users/sdce/Documents/GitHub/membership-card-generator/member_gen_app/application/uploads"
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 # Get abslute path to base image filder
-BASE_IMAGE_DIR = "/Users/sdce/Desktop/acgl/membership_card_gen/member_gen_app/application/asset/base"
+BASE_IMAGE_DIR = "/Users/sdce/Documents/GitHub/membership-card-generator/member_gen_app/application/asset/base"
 # Path to members saving filder
-MEMBER_IMAGE_DIR = "/Users/sdce/Desktop/acgl/membership_card_gen/members"
+MEMBER_IMAGE_DIR = "/Users/sdce/Documents/GitHub/membership-card-generator/members"
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = "/Users/sdce/Desktop/acgl/membership_card_gen/member_gen_app/application/uploads"
+app.config['UPLOAD_FOLDER'] = "/Users/sdce/Documents/GitHub/membership-card-generator/member_gen_app/application/uploads"
 
 from application import routes
 
